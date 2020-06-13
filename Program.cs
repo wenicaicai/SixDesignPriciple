2f722e895c9786d99969e152a90707df08e815bb
杨永财 <youngwenicai@outlook.com>
2020-06-14 00:16:57 +0800

    1.learning Inherit 2.Learnig Polymorphic

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Inherit;
+using Polymorphic;
+using System;
 
 namespace SixDesignPrinciple
 {
@@ -6,13 +8,24 @@ namespace SixDesignPrinciple
     {
         static void Main(string[] args)
         {
-            var dip= new DIP();
+            var dip = new DIP();
             var CTBook = new Book();
             var WCNewspaper = new NewsPaper();
             dip.ReadStore(CTBook);
             dip.ReadStore(WCNewspaper);
             Console.WriteLine("Hello World!");
+
+            //多态
+            IPerson programmer = new Programmer();
+            programmer.DoWork();
+            IPerson myfanther = new Father();
+            myfanther.DoWork();
             Console.ReadKey();
+
+            //测试继承（鸡类、鹰类）
+            Bird bird = new Bird();
+            Chicken chicken = new Chicken();
+            //继承时，发生创建类的过程，方法列表、属性列表，父类的先于子类，先于对象创建
         }
     }
 }
