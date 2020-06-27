9e7d98c7094678e0494fb5338f0df11e8da40f88
杨永财 <youngwenicai@outlook.com>
2020-06-27 23:17:23 +0800

    1.Enum Intro

---
+++ Days.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace SixDesignPrinciple
+{
+    public enum Days : Byte
+    {
+        [Description("周一")]
+        Monday = 1,
+
+        [Description("周二")]
+        Tuesday = 2,
+
+        [Description("周三")]
+        Wednesday = 3,
+
+        [Description("周四")]
+        Thursday = 4,
+
+        [Description("周五")]
+        Friday = 5,
+
+        [Description("周六")]
+        Saturday = 6,
+
+        [Description("周日")]
+        Sunday = 7
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using Inherit;
+﻿using Common;
+using Inherit;
+using Models;
 using Polymorphic;
 using System;
 
@@ -24,11 +26,54 @@ namespace SixDesignPrinciple
 
             //测试继承（鸡类、鹰类）
             Bird bird = new Bird();
-            
+
             Chicken chicken = new Chicken();
             BirdAdapter birdAdapter = new BirdAdapter(chicken);
             birdAdapter.ToTweet();
             //继承时，发生创建类的过程，方法列表、属性列表，父类的先于子类，先于对象创建
+
+            //枚举的转换
+            var foo = Days.Sunday.ToString();
+            Console.WriteLine(foo);
+
+            var goo = Enum.GetName(typeof(Days), 6);
+
+            var hoo = Enum.TryParse("Tuesday", out Days bar);
+
+            var too = (Days)Enum.Parse(typeof(Days), "Tuesday");
+
+            var uoo = (byte)Days.Monday;
+
+            var eoo = (Days)2;
+
+            Type yoo = Enum.GetUnderlyingType(typeof(Days));
+
+            var woo = Enum.GetValues(typeof(Days));
+
+            var noo = Enum.GetNames(typeof(Days));
+
+            var poo = (Days)21;
+
+            Enum.IsDefined(typeof(Days), poo);
+
+            var doo = default(Days);
+
+            //枚举可以通过Description、Display等特性来为成员添加有用的辅助信息
+
+            var user = new User { Id = 1, Roles = Common.Roles.Admin };
+            /**
+             * 枚举的Flags特性
+             */
+            if (user.Roles.HasFlag(Roles.Admin))
+            {
+                Console.WriteLine("I am Admin.");
+            }
+
+            //这是什么操作
+            var ioo = (Roles.Admin | Roles.Member);
+
+            var par = ioo & ~ioo;
+
         }
     }
 }
